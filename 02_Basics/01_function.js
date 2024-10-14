39447fc5092fa26e0239ac07e87a4771bf497c2e
Akashmahto022 <akashmahto1169@gmail.com>
2024-10-14 22:07:33 +0530

    learn about function in ts

---
+++ 02_Basics/01_function.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(value) {
+    return value.toUpperCase();
+}
+function signupUser(email, password, isPaid) {
+}
+var loginUser = function (email, password, isPaid) {
+};
+loginUser("one@gmail.com", "jhgfds", false);
+signupUser("kabca", "12345", true);
+getUpper("akash mahto");
+addTwo(10);
---
+++ 02_Basics/01_function.ts
@@ -0,0 +1,25 @@
+function addTwo(num:number){
+    return num + 2
+}
+
+function getUpper(value:string){
+    return value.toUpperCase()
+}
+
+function signupUser(email:string, password:string, isPaid:boolean){
+
+}
+
+const loginUser = (email:string, password:string, isPaid:boolean)=>{
+
+}
+
+loginUser("one@gmail.com", "jhgfds", false)
+
+signupUser("kabca", "12345", true)
+getUpper("akash mahto")
+
+addTwo(10)
+
+export{}
+
