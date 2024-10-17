74a16975dd1312222dfc249fbd960072c3b54da7
Akashmahto022 <akashmahto1169@gmail.com>
2024-10-17 10:08:43 +0530

    readonly and optional keyword in ts

---
+++ 02_Basics/03_array.ts
@@ -0,0 +1,17 @@
+const superHeros: object[] = []
+
+type User = {
+    name: string;
+    isActive: boolean
+}
+
+const allUser: User[] = []
+
+allUser.push({name: "akash", isActive: true})
+
+const MLModels: number[][] = [
+    [555, 2525, 4122],
+    []
+]
+
+superHeros.push({ name: "Spiderman", power: 123 })
