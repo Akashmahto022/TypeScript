cca6619006d93b28e02e67d1e2164f43f8224451
Akashmahto022 <akashmahto1169@gmail.com>
2024-10-17 10:01:17 +0530

    readonly and optional keyword in ts

---
+++ 02_Basics/02_objects.js
@@ -0,0 +1,18 @@
+"use strict";
+// const user = {
+//     name: "Akash",
+//     email: "one@gmail.com",
+//     isActive: true
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+function createUser(user) {
+}
+var myUser = {
+    _id: "er78ijnb",
+    name: "bnm",
+    email: "dfghn",
+    isActive: true,
+};
+myUser.email = "456ygbcrtyujbfryh";
+// myUser._id="213ds"
+createUser({ _id: "34567890876rfvb", name: "", email: "", isActive: true });
--- 02_Basics/02_objects.ts
+++ 02_Basics/02_objects.ts
@@ -1,20 +1,80 @@
-const user = {
-    name: "Akash",
-    email: "one@gmail.com",
-    isActive: true
+// const user = {
+//     name: "Akash",
+//     email: "one@gmail.com",
+//     isActive: true
+// }
+
+// function createUser({name: string, isActive: boolean}){}
+
+// let newUser = {name: "Akash", isActive: false}
+
+// createUser(newUser)
+
+
+
+// function createCourse():{name: string, price: number}{
+//     return {name: "reactJs", price: 999}
+// }
+
+
+// type User = {
+//     name: string;
+//     email: string;
+//     isActive: boolean;
+// }
+
+// function createUser(user:User):User{
+//     return {}
+// }
+
+// createUser({name:"Akash", email: "one@gmil.com", isActive:true})
+
+
+type User = {
+    readonly _id: string
+    name: string;
+    email: string;
+    isActive: boolean;
+    credcardDetails?: number
 }
 
-function createUser({name: string, isActive: boolean}){}
+function createUser(user: User){
 
-let newUser = {name: "Akash", isActive: false}
-
-createUser(newUser)
-
-
-
-function createCourse():{name: string, price: number}{
-    return {name: "reactJs", price: 999}
 }
+
+let myUser: User = {
+    _id: "er78ijnb",
+    name: "bnm",
+    email: "dfghn",
+    isActive: true,
+}
+
+type cardNumber ={
+    cardnumber: string
+}
+
+type cardDate = {
+    cardDate: string
+}
+
+type cardDetails = cardNumber & cardDate & {
+    cvv: number
+}
+
+
+
+
+myUser.email = "456ygbcrtyujbfryh"
+
+// myUser._id="213ds"
+
+createUser({_id: "34567890876rfvb", name: "", email: "", isActive: true})
+
+
+
+
+
+
 
 export{}
 
