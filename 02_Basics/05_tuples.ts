83303fc0960134019f94802e2a081b600b6d1ab4
Akashmahto022 <akashmahto1169@gmail.com>
2024-10-17 11:12:33 +0530

    tuples in the ts

---
+++ 02_Basics/05_tuples.ts
@@ -0,0 +1,19 @@
+// const user: (string | number)[] = ["AK", 21]
+
+let user: [string, number, boolean]
+
+user = ['AK', 21, true]
+
+let rgb: [number,number, number]
+
+rgb = [255, 123, 112]
+rgb = [355, 523, 712]
+
+type User = [number, string]
+
+const newUser: User = [21, "Akash"]
+
+newUser[0]= 1
+newUser.push(true)
+
+
